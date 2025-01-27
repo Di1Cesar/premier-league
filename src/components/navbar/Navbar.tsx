169a0332fff900c677dfa731bcedd158d371933c
dioni-dcrr <dioni-dcrr@defensoria.go.def.br>
2025-01-27 17:38:22 -0300

    Estrutura inicial

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,127 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import Times from "../../static-data/Times";
+
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <>
+      <AppBar style={{backgroundColor: '#ffcd00'}} position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            {/* Mobile Menu */}
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{ display: { xs: "block", md: "none" } }}
+              >
+                {Times.map((time) => (
+                  <MenuItem key={time.nome} onClick={handleCloseNavMenu}>
+                    <img
+                      src={time.foto}
+                      alt={time.nome}
+                      style={{ width: "24px", height: "24px", marginRight: "8px" }}
+                    />
+                    <Typography sx={{ textAlign: "center" }}>{time.nome}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            {/* Mobile Logo */}
+            
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              <img width={'43.5px'} src="https://www.cbf.com.br/img/logo/logo-borda.png" alt="" />
+            </Typography>
+
+            {/* Desktop Navigation Items (Images) */}
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {Times.map((time) => (
+                <IconButton
+                  key={time.nome}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  <img
+                    src={time.foto}
+                    alt={time.nome}
+                    style={{ width: "30px", height: "35px" }}
+                  />
+                </IconButton>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/static-data/Times.tsx
@@ -0,0 +1,26 @@
+const Times = [
+
+    {nome: 'Atlético Mineiro', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Atletico_mineiro_galo.png/120px-Atletico_mineiro_galo.png'},
+    {nome: 'Bahia', foto: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/90/ECBahia.png/130px-ECBahia.png'},
+    {nome: 'Botafogo FR', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Botafogo_de_Futebol_e_Regatas_logo.svg/120px-Botafogo_de_Futebol_e_Regatas_logo.svg.png'},
+    {nome: 'Ceará', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Cear%C3%A1_Sporting_Club_logo.svg/120px-Cear%C3%A1_Sporting_Club_logo.svg.png'},
+    {nome: 'Lixo', foto: 'https://upload.wikimedia.org/wikipedia/pt/thumb/b/b4/Corinthians_simbolo.png/120px-Corinthians_simbolo.png'},
+    {nome: 'Cruzeiro', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Cruzeiro_Esporte_Clube_%28logo%29.svg/120px-Cruzeiro_Esporte_Clube_%28logo%29.svg.png'},
+    {nome: 'Flamengo', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Flamengo_braz_logo.svg/120px-Flamengo_braz_logo.svg.png'},
+    {nome: 'Fluminense', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/FFC_crest.svg/130px-FFC_crest.svg.png'},
+    {nome: 'Fortaleza', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Fortaleza_EC_2018.png/120px-Fortaleza_EC_2018.png'},
+    {nome: 'Grêmio', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Gremio_logo.svg/120px-Gremio_logo.svg.png'},
+    {nome: 'Internacional', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/SC_Internacional_Brazil_Logo.svg/120px-SC_Internacional_Brazil_Logo.svg.png'},
+    {nome: 'Juventude', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/EC_Juventude.svg/140px-EC_Juventude.svg.png'},
+    {nome: 'Mirassol', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Mirassol_FC_logo.png/150px-Mirassol_FC_logo.png'},
+    {nome: 'S.E. Palmeiras', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Palmeiras_logo.svg/120px-Palmeiras_logo.svg.png'},
+    {nome: 'RB Bragantino', foto: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9e/RedBullBragantino.png/140px-RedBullBragantino.png'},
+    {nome: 'Santos', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Santos_Logo.png/120px-Santos_Logo.png'},
+    {nome: 'São Paulo', foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Brasao_do_Sao_Paulo_Futebol_Clube.svg/120px-Brasao_do_Sao_Paulo_Futebol_Clube.svg.png'},
+    {nome: 'Sport', foto: 'https://upload.wikimedia.org/wikipedia/pt/thumb/1/17/Sport_Club_do_Recife.png/120px-Sport_Club_do_Recife.png'},
+    {nome: 'Vasco', foto: 'https://upload.wikimedia.org/wikipedia/pt/thumb/a/ac/CRVascodaGama.png/120px-CRVascodaGama.png'},
+    {nome: 'Vitória', foto: 'https://upload.wikimedia.org/wikipedia/pt/3/34/Esporte_Clube_Vit%C3%B3ria_logo.png'},
+    
+]
+
+export default Times;
