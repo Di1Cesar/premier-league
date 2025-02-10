826934f134aec72b7c9fbb5283ab6611ddcbde2a
Dioni Cesar <dionecesar99@gmail.com>
2025-02-10 07:28:41 -0300

    fix: atualização de pacote

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Principal from "./Pages/Index";
 import TelaJogadores from "./Pages/TelaJogadores";
 import TelaTimes from "./Pages/TelaTimes";
 
-
 function App() {
   return (
-    <>
-    <TelaTimes/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Principal />} />
+        <Route path="/jogadores" element={<TelaJogadores />} />
+        <Route path="/times" element={<TelaTimes />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -1,6 +1,8 @@
 import { Box, Container } from "@mui/material";
 import Navbar from "../components/navbar/Navbar";
-import { Button, Flex, Typography } from "antd";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import Footer from "../components/footer";
 
 export default function Principal() {
   return (
@@ -16,14 +18,19 @@ export default function Principal() {
       >
         <Container style={{ display: "flex", alignItems: "center" }}>
           <Button style={{ height: "15em", width: "70em" }} type="primary">
-            <h1>Times</h1>
+            <Link to={"/times"}>
+              <h1>Times</h1>
+            </Link>
           </Button>
           <span style={{ width: "2em" }}></span>
           <Button style={{ height: "15em", width: "70em" }} type="primary">
-            <h1>Jogadores</h1>
+            <Link to={"/jogadores"}>
+              <h1>Jogadores</h1>
+            </Link>
           </Button>
         </Container>
       </Box>
+      <Footer/>
     </>
   );
 }
