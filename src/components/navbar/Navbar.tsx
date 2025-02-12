652f605d050620215a16716e7a8bc6d84c2651ff
Dioni Cesar <dionecesar99@gmail.com>
2025-02-12 15:50:27 -0300

    fix: navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,5 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
@@ -30,34 +20,12 @@ export default function DrawerAppBar(props: Props) {
     setMobileOpen((prevState) => !prevState);
   };
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
 
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        fontFamily: "oxanium",
-        backgroundColor: "#0d67e7",
-      }}
-    >
-      <CssBaseline />
-      <AppBar
+      <Box
         className="menu"
         component="nav"
         sx={{ backgroundColor: "#0d67e7", color: "#fff" }}
@@ -68,45 +36,24 @@ export default function DrawerAppBar(props: Props) {
             component="div"
             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            <img src="https://www.cbf.com.br/logo/logo-verde.svg" alt="" />
+           <Link to="/"> <img src="https://www.cbf.com.br/logo/logo-verde.svg" alt="" /> </Link>
           </Typography>
           <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
               <Button key={item} sx={{ color: "#FFFFFF", fontFamily: "oxanium" }}>
                 {item === "Home" ? (
-                  <Link to="/">Home</Link>
+                  <Link to="/" style={{color: '#FFFFFF'}}>Home</Link>
                 ) : item === "Clubes" ? (
-                  <Link to="/times">Clubes</Link>
+                  <Link to="/times" style={{color: '#FFFFFF'}}>Clubes</Link>
                 ) : item === "Jogadores" ? (
-                  <Link to="/jogadores">Jogadores</Link>
+                  <Link to="/jogadores" style={{color: '#FFFFFF'}}>Jogadores</Link>
                 ) : (
-                  ""
+                  
                 )}
               </Button>
             ))}
           </Box>
         </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-    </Box>
+      </Box>
   );
 }
